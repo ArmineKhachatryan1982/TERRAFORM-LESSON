1c7b8bc68b3d02dcac74be750abc48f91e809b04
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-03-12 19:31:42 +0400

    add_backet

--- main.tf
+++ main.tf
@@ -1,32 +1,9 @@
 resource "aws_instance" "web" {
+  count         = 3
   ami           = "ami-022661f8a4a1b91cf"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
-  }
-}
-resource "aws_instance" "web1" {
-  ami           = "ami-022661f8a4a1b91cf"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "MyEc2-1"
-  }
-}
-resource "aws_instance" "web2" {
-  ami           = "ami-022661f8a4a1b91cf"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "MyEc2-2"
-  }
-}
-resource "aws_instance" "web3" {
-  ami           = "ami-022661f8a4a1b91cf"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "MyEc2-3"
+    Name = "MyEc2-${count.index + 1}"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "mybucketteraform"
+    key            = "tfstate/state"
+    region         = "us-east-2"
+    dynamodb_table = "my-lock-table"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
